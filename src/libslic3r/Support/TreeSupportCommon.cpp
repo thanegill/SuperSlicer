ef846ac6c74634e6726c55c5b2f463800b45b956
Merill <merill@free.fr>
2024-09-26 08:31:51 +0200

    Fix bad usage of "min_feature_size"

--- src/libslic3r/Support/TreeSupportCommon.cpp
+++ src/libslic3r/Support/TreeSupportCommon.cpp
@@ -33,16 +33,16 @@ TreeSupportMeshGroupSettings::TreeSupportMeshGroupSettings(const PrintObject &pr
     assert(config.support_material_style == smsTree || config.support_material_style == smsOrganic);
 
     // Calculate maximum external perimeter width over all printing regions, taking into account the default layer height.
-    coordf_t external_perimeter_width = 0.;
+    double external_perimeter_width = 0.;
     for (size_t region_id = 0; region_id < print_object.num_printing_regions(); ++ region_id) {
         const PrintRegion &region = print_object.printing_region(region_id);
-        external_perimeter_width = std::max<coordf_t>(external_perimeter_width, region.flow(print_object, frExternalPerimeter, config.layer_height).width());
+        external_perimeter_width = std::max<double>(external_perimeter_width, region.flow(print_object, frExternalPerimeter, config.layer_height).width());
     }
 
     this->layer_height              = scaled<coord_t>(config.layer_height.value);
     this->resolution                = scaled<coord_t>(print_config.gcode_resolution.value);
     // Arache feature
-    this->min_feature_size          = scaled<coord_t>(config.min_feature_size.value);
+    this->min_feature_size          = scaled<coord_t>(external_perimeter_width * 0.25);
     // +1 makes the threshold inclusive
     this->support_angle             = 0.5 * M_PI - std::clamp<double>((config.support_material_threshold + 1) * M_PI / 180., 0., 0.5 * M_PI);
     this->support_line_width        = support_material_flow(&print_object, config.layer_height).scaled_width();
